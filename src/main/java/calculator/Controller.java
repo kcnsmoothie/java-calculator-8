048204a1f431381eb7c54a176df362af205fbbe6
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-10-16 03:07:39 +0900

    feat: 커스텀 구분자로 구분 기능 구현

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -10,5 +10,15 @@ public class Controller {
         // 기본 구분자(쉼표,컬럼)로 문자열 분리
         String defaultDelimiter = ",|:";
         String[] parsedList = inputStr.split(defaultDelimiter);
+
+        //커스텀 구분자로 문자열 분리
+        String customDelimiter;
+        int endOfCustomDelimiter;
+        if (inputStr.startsWith("//") && inputStr.contains("|n")) {
+            //커스텀 구분자가 끝나는 문자열의 index 위치 반환
+            endOfCustomDelimiter = inputStr.indexOf("|");
+            customDelimiter = inputStr.substring(2, endOfCustomDelimiter);
+            System.out.println(customDelimiter);
+        }
     }
 }
