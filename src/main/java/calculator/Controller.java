c444b7fda65aaa241099651ea7d778c295492d9c
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-10-15 23:06:54 +0900

    feat: 기본 구분자로 구분 기능 구현

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -6,5 +6,9 @@ public class Controller {
     public void input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputStr = Console.readLine();
+
+        // 기본 구분자(쉼표,컬럼)로 문자열 분리
+        String defaultDelimiter = ",|:";
+        String[] parsedList = inputStr.split(defaultDelimiter);
     }
 }
