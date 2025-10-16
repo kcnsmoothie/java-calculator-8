b9fd8f3c1cf415546ae77c4618faa4374cd330a7
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-10-16 14:26:52 +0900

    refactor: 입력과 입력값 분리 메서드 분할

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -3,22 +3,34 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Controller {
-    public void input() {
+    private String inputStr;
+    private String delimiter = ",|:";
+    private String customDelimiter = "";
+    private int endOfCustomDelimiter;
+
+    public String Input() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String inputStr = Console.readLine();
+        inputStr = Console.readLine();
+        return inputStr;
+    }
 
-        // 기본 구분자(쉼표,컬럼)로 문자열 분리
-        String defaultDelimiter = ",|:";
-        String[] parsedList = inputStr.split(defaultDelimiter);
+    public void inputParse() {
+        Input();
+        System.out.println(inputStr);
 
-        //커스텀 구분자로 문자열 분리
-        String customDelimiter;
-        int endOfCustomDelimiter;
         if (inputStr.startsWith("//") && inputStr.contains("|n")) {
             //커스텀 구분자가 끝나는 문자열의 index 위치 반환
             endOfCustomDelimiter = inputStr.indexOf("|");
+            //사용자가 지정한 커스텀 구분자를 저장
             customDelimiter = inputStr.substring(2, endOfCustomDelimiter);
-            System.out.println(customDelimiter);
+            // 구분자 안에 커스텀 구분자를 추가
+            delimiter = delimiter.concat("|").concat(customDelimiter);
         }
+
+        System.out.println(delimiter);
+
+        //구분자로 정수 파싱 및 배열 저장
+        String[] parsedList = inputStr.split(delimiter);
+        System.out.println(parsedList.length);
     }
 }
